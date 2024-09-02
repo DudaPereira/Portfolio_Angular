c77a3057d8bb658b90ba5b1c99083d49595ac1fc
Maria Eduarda <maria.epereira@g.globo>
2024-09-01 23:53:43 -0300

    adicionado projetos

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -24,17 +24,49 @@ export class ProjectsComponent {
 
   public arrayProjects = signal<IProjects[]>([
     {
-      src: '',
+      src: 'assets/img/projects/login-dark-light-mode.png',
       alt: 'Projeto',
-      title: 'Nome Projeto',
-      with: '100px',
-      height: '51px',
+      title: 'Tela de login dark and light mode',
+      with: '150px',
+      height: '150px',
       description:
-        '<p>Descricao</p>',
+        '<p>Este projeto é uma interface de tela de login que permite aos usuários alternar entre os modos claro e escuro. Ferramentas utilizadas HTML, CSS e JavaScript</p>',
       links: [
         {
           name: 'Conheça',
-          href: '',
+          href: 'https://logindarklightmode.netlify.app',
+        },
+      ],
+    },
+
+    {
+      src: 'assets/img/projects/card-movies.png',
+      alt: 'Projeto',
+      title: 'Catálogo de Filmes Interativo',
+      with: '200px',
+      height: '150px',
+      description:
+        '<p>Este é um projeto web simples de um catálogo de filmes, onde cada filme é exibido como um card interativo. Os cards se expandem para revelar mais detalhes sobre o filme quando clicados. Ferramentas utilizadas HTML, CSS e JavaScript</p>',
+      links: [
+        {
+          name: 'Conheça',
+          href: 'https://cardmovies.netlify.app',
+        },
+      ],
+    },
+
+    {
+      src: 'assets/img/projects/e-players.png',
+      alt: 'Projeto',
+      title: 'E-Players',
+      with: '200px',
+      height: '100px',
+      description:
+        '<p>Este foi meu primeiro projeto desenvolvido no curso técnico de Desenvolvimento de Sistemas. Ferramentas utilizadas HTML e CSS</p>',
+      links: [
+        {
+          name: 'Conheça',
+          href: 'https://e-players.netlify.app',
         },
       ],
     },
